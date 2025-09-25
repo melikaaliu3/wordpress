f1b32434bfe9d078bb39393182880fb4e094b593
melikaaliu3 <melikaaliu3@gmail.com>
2025-09-25 07:02:51 -0700

    wordpress

---
+++ page-about.php
@@ -0,0 +1,49 @@
+<?php get_header();?>
+
+<div id="content" class="site-content">
+    <main id="main" class="site-main">
+        <section class="about-hero">
+            <div class="container">
+                <h1>About us</h1>
+                <p>Welcome to our tech corner-where innovation meets explanation</p>
+                </div>
+        </section>
+
+        <section class="about-info">
+            <div class="container">
+                <h2>We are a team of developers engineers and tech enthusiats who love to write about the fast-moving world technology</p>
+                </div>
+</section>
+
+<section class="about-mission">
+    <div class="container">
+        <h2>Our Mission</h2>
+        <p>Our goal is simple:to empower curios minds and passionate techies by delivering accurate.useful and accessible content</p>
+        </div>
+        
+    </section>
+
+    <section class="about-what-we-cover">
+        <div class="container">
+            <h2>What We Cover</h2>
+            <ul>
+                <li>Programming tutorials and tips</li>
+                <li>software and app review</li>
+                <li>AI/ML and data science insights</li>
+                <li>Web development, devops and cloud computinf </li>
+                <li>Web development, devops and cloud computinf </li>
+                <li>Trends in tech adn industry news </li>
+            </ul>
+        </div>
+        </section>
+
+        <section class="about-contact">
+            <div class="container">
+                <h2>Want to connect</h2>
+                <p>We love to hear from our readers whether you have a question suggestion or just want to say hello feel free to reach out to us at </p>
+</div>
+        </section>
+
+    </main>
+
+</div>
---
+++ searchform.php
@@ -0,0 +1,9 @@
+<form role="search" method="get" class="searchform" action="<?php echo esc_url(home_url('/')); ?>">
+    <div>
+        <label class="screen-reader-text" for="s">Search form:</label>
+        <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" >>
+        <input type="submit" id="searchsubmit" value="Search" />
+</div>
+        
+</form>
+ 
